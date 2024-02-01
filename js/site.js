22d1d163ca72bc20e664dac8be0cd888957de9a2
lilacwasplanted <lhollo5823@gmail.com>
2024-01-31 19:55:37 -0700

    7:55

---
+++ js/site.js
@@ -0,0 +1,22 @@
+const GamesIvePlayed = document.getElementById("GamesIvePlayed");
+let imgsToUse =[
+    "images/StrangeHorticulture.webp",
+    "images/EpicMickey2.PNG",
+    "images/Fortnite.png",
+    "images/hearthstone.jpg",
+    "images/PotionCraft.webp",
+    "images/Sims4.png",
+    "images/minecraft.jpg"
+
+];
+let currentImageIndex = 0;
+function updateSlideShowImage(){
+    currentImageIndex++
+
+    if(currentImageIndex >= imgsToUse.length){
+        currentImageIndex = 0;
+    }
+    let nextImageURL = imgsToUse[currentImageIndex];
+    GamesIvePlayed.src = nextImageURL;
+}
+setInterval(updateSlideShowImage, 2500);
