a137001cd0dc5fcd941e48ca2316c2ccbbcc9e39
lilacwasplanted <lhollo5823@gmail.com>
2024-02-01 00:44:11 -0700

    2/1, 12:45

--- js/site.js
+++ js/site.js
@@ -1,13 +1,12 @@
 const GamesIvePlayed = document.getElementById("GamesIvePlayed");
-let imgsToUse =[
-    "images/StrangeHorticulture.webp",
-    "images/EpicMickey2.PNG",
-    "images/Fortnite.png",
-    "images/hearthstone.jpg",
-    "images/PotionCraft.webp",
-    "images/Sims4.png",
-    "images/minecraft.jpg"
-
+let imgsToUse = [
+    "./images/StrangeHorticulture.webp",
+    "./images/EpicMickey2.PNG",
+    "./images/Fortnite.png",
+    "./images/hearthstone.jpg",
+    "./images/PotionCraft.webp",
+    "./images/Sims4.png",
+    "./images/minecraft.jpg"
 ];
 let currentImageIndex = 0;
 function updateSlideShowImage(){
@@ -20,3 +19,15 @@ function updateSlideShowImage(){
     GamesIvePlayed.src = nextImageURL;
 }
 setInterval(updateSlideShowImage, 2500);
+
+
+
+
+const theFUNbutton = document.getElementById("theFUNbutton");
+const theFUNaudio = document.getElementById("theFUNaudio");
+
+ function partyTime(){
+    alert("Time to have PARTTYYYY")
+theFUNaudio.play();
+ }
+ theFUNbutton.addEventListener('click',partyTime);
